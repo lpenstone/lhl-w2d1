9ea619a71c927df40f57881ad5183037a5245d3c
lpenstone <laura.penstone@queensu.ca>
2017-07-03 15:39:34 +0000

    making the code more modular

---
+++ step2.js
@@ -0,0 +1,32 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-practice/step2.html'
+  };
+
+// notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    var dataString = '';
+    for (var i = 0; i < 1; i++){
+    dataString += data + '\n';
+    }
+    console.log(dataString);
+  });
+    response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+});
+
+}
+
+getAndPrintHTML();
---
+++ step3.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-practice/step3.html'
+  };
+
+
+function getAndPrintHTML (options) {
+
+// notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    var dataString = '';
+    for (var i = 0; i < 1; i++){
+    dataString += data + '\n';
+    }
+    console.log(dataString);
+  });
+    response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+});
+
+}
+
+getAndPrintHTML(requestOptions);
